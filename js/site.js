f96a6095636658683728d0ecf6c29d5e1c684d61
mberchillc <mberchillc@gmail.com>
2026-06-18 23:11:45 -0400

    Refine hero portrait and masthead

--- js/site.js
+++ js/site.js
@@ -1,2 +1,4 @@
 document.querySelectorAll('[data-menu]').forEach(button=>button.addEventListener('click',()=>{const nav=document.querySelector('.nav');const open=nav.classList.toggle('open');button.setAttribute('aria-expanded',String(open))}));
 document.querySelectorAll('[data-language]').forEach(link=>link.addEventListener('click',()=>localStorage.setItem('ts-language',link.dataset.language)));
+
+if(document.querySelector('.hero')){const refinements=document.createElement('link');refinements.rel='stylesheet';refinements.href=new URL('../css/hero-refinements.css',document.currentScript.src).href;document.head.appendChild(refinements)}
